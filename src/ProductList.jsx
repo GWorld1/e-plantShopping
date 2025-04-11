b4f233605cc759b5689f31c22576689595e2d19f
Sans <sans@example.com>
2025-04-11 17:56:10 -0400

    updated productList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+import { useDispatch } from 'react-redux';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -233,6 +238,14 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
+
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -271,10 +284,33 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
-            </div>
+            </div>       
+               
+            
             {!showCart ? (
+                
                 <div className="product-grid">
-
+                    
+                    {
+                        plantsArray.map((category,index)=>(
+                            <section key={index}>
+                                <h1 style={{textAlign:'center'}}>{category.category}</h1>
+                                <div className={'product-list'}>
+                                  {
+                                    category.plants.map((plant,plantIndex)=>(
+                                        <div key={plantIndex} className='product-card'>
+                                            <p  className="product-title">{plant.name}</p>
+                                            <img className="product-image" src={plant.image} />
+                                            <p className="product-price">{plant.cost}</p>
+                                            <p>{plant.description}</p>
+                                            <button  className="product-button">Add to Cart</button>
+                                        </div> 
+                                    ))
+                                }
+                                </div>
+                            </section>
+                        ))
+                    }
 
                 </div>
             ) : (
